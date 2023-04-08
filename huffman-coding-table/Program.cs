627b7579f668b343d1ad53096960b10edbf6ce73
Sofiia Tyshchenko <styshchenko@kse.org.ua>
2023-04-08 10:51:34 +0300

    Added draft for Huffman Tree

--- huffman-coding-table/Program.cs
+++ huffman-coding-table/Program.cs
@@ -2,17 +2,51 @@
 using System.IO;
 using System.Collections.Generic;
 
+
 class Program
 {
     static void Main(string[] args)
     {
         // Change the file to your directory
-        string filePath = "/Users/antoninanovak/RiderProjects/huffman-coding-table/huffman-coding-table/sherlock.txt";
+        string filePath = "D:/Навчання/Терм ІІІ/Прикладні алгоритми та структури даних І/huffman-coding-table/huffman-coding-table/sherlock.txt";
         Dictionary<char, int> characterFrequency = CountCharacterFrequency(filePath);
         
         foreach (KeyValuePair<char, int> entry in characterFrequency)
         {
             Console.WriteLine($"Character: {entry.Key}, Count: {entry.Value}");
+        }
+        
+        // sort (from min to max) characters by their frequency 
+        Console.WriteLine($"\nSORTED");
+        
+        foreach (KeyValuePair<char, int> entry in characterFrequency.OrderBy(key => key.Value))
+        {
+            Console.WriteLine($"Character: {entry.Key}, Count: {entry.Value}");
+        }
+
+        Node root = HuffmanTree(characterFrequency);
+        
+    }
+
+    public class Node
+    {
+        private char? Character;
+        private int Frequency;
+        private Node rightChild;
+        private Node leftChild; 
+        
+        
+        public Node(char? Character, int Frequency)
+        {
+            this.Character = Character;
+            this.Frequency = Frequency;
+        }
+
+        public Node(Node rightChild, Node leftChild, int Frequency)
+        {
+            this.rightChild = rightChild;
+            this.leftChild = leftChild;
+            this.Frequency = Frequency;
         }
     }
 
@@ -37,5 +71,34 @@ class Program
         }
 
         return characterFrequency;
+        
     }
+    
+    Node HuffmanTree(int[] characterFrequency)
+    {
+        var PriorityQueue = new PriorityQueue<Node>();
+        for (int i = 0; i < 1000; i++)
+        {
+            if (characterFrequency[i] > 1)
+            {
+                PriorityQueue.Enqueue(characterFrequency[i], new Node(characterFrequency[i]));
+            }
+        }
+
+        while (PriorityQueue.Count > 1)
+        {
+            Node rightChild = PriorityQueue.Dequeue(); 
+            Node leftChild = PriorityQueue.Dequeue();
+            var currentFrequency = rightChild.Frequency + leftChild.Frequency;
+            Node nextNode = new Node(rightChild, leftChild, currentFrequency);
+            PriorityQueue.Enqueue(nextNode, currentFrequency);
+            
+        }
+
+        return PriorityQueue.Dequeue(Node);
+
+    }
+    
+   
+    
 }
